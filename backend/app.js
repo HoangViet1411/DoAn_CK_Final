c1f22f1cf744d60cd3ab9cba0a7ed1f1c528d76e
HoangViet1411 <vietdai47@gmail.com>
2025-04-06 21:58:46 +0700

    update final

--- backend/app.js
+++ backend/app.js
@@ -4,31 +4,28 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const categoriesRoutes = require('./routes/categories');
-const productRoutes = require("./routes/products");
-const orderRoutes = require("./routes/orders");
+const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
 const cartRoutes = require('./routes/carts');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads')); // Thêm middleware phục vụ tệp tĩnh
+app.use('/uploads', express.static('uploads'));
 
 // Mount Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoriesRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 app.use('/api/carts', cartRoutes);
 
-
-// === Database Connection ===
+// Database Connection
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://admin:123@cluster0.xo2tj.mongodb.net/laptop_web';
 mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB đã kết nối thành công.'))
   .catch(err => console.error('Lỗi kết nối MongoDB:', err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server đang chạy trên cổng ${PORT}`);
-});
+// Export app cho Vercel
+module.exports = app;
